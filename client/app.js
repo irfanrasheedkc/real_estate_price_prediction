0d4963bbbfd7c74940ba200cac55a8b85f0d612e
irfan rasheed <irfanrasheed273@gmail.com>
2022-11-10 19:57:58 +0530

    Website deployed

--- client/app.js
+++ client/app.js
@@ -28,8 +28,8 @@ function onClickedEstimatePrice(){
     console.log("location is :"+document.getElementById("uiLocations").value);
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    var url = "http://127.0.0.1:5000/predict_home_price";
-
+    // var url = "http://127.0.0.1:5000/predict_home_price";
+    var url = "/api/predict_home_price";
     $.post(url , {
         total_sqft:parseFloat(sqft),
         bhk:bhk,
@@ -44,7 +44,8 @@ function onClickedEstimatePrice(){
 
 function onPageLoad(){
     console.log("Document loaded");
-    var url = "http://127.0.0.1:5000/get_location_names";
+    // var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "/api/get_location_names";
     $.get(url,function(data , status){
         console.log("got response for get_location_names request");
         if(data){
